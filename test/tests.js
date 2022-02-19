b27952f0fb01c65fa61407eca4bda35b1a2ff101
rupa <rupa@lrrr.us>
2022-02-19 13:53:42 -0500

    tests

--- test/tests.js
+++ test/tests.js
@@ -8,6 +8,9 @@ const {expect} = require("chai");
 const {cuecat, getInfoAndChunks} = require("../cuecat");
 
 const testDir = "./test/CR78"
+const stereoFile = "./test/stereo.wav"
+const cueHash = "9228fc19a3cec3518a177d31bc0d8c74"
+
 const files = fs.readdirSync(testDir)
     .filter(f => path.extname(f).toLowerCase() === ".wav")
     .map(f => `${testDir}/${f}`)
@@ -17,7 +20,11 @@ describe("cuecat", () => {
         const wav = cuecat(files)
         const hash = crypto.createHash('md5')
             .update(wav.toBuffer()).digest("hex")
-        expect(hash).to.equal("9228fc19a3cec3518a177d31bc0d8c74")
+        expect(hash).to.equal(cueHash)
+        expect(wav.bitDepth).to.equal('16')
+        expect(wav.fmt.numChannels).to.equal(1)
+        expect(wav.fmt.sampleRate).to.equal(44100)
+        expect(wav.cue.dwCuePoints).to.equal(16)
     })
 });
 
@@ -33,5 +40,17 @@ describe("getInfoAndChunks", () => {
         expect(maxSamples).to.equal(27082)
         expect(sumSamples).to.equal(219311)
         expect(chunks).to.have.length(16)
+    })
+    it("should be stereo if any file is stereo", () => {
+        const {
+            maxChannels,
+            maxSamples,
+            sumSamples,
+            chunks
+        } = getInfoAndChunks([...files, stereoFile], '16', 44100.0)
+        expect(maxChannels).to.equal(2)
+        expect(maxSamples).to.equal(123070)
+        expect(sumSamples).to.equal(342381)
+        expect(chunks).to.have.length(17)
     })
 })
