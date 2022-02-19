e1d027040c0ba04a3d4f805c4907cfe503318da6
rupa <rupa@lrrr.us>
2022-02-19 03:35:55 -0500

    factor out a function

--- cuecat.js
+++ cuecat.js
@@ -3,26 +3,31 @@
 const fs = require('fs')
 const wavefile = require('wavefile')
 
-const cuecat = (files, bitDepth='16', sampleRate=44100.0) => {
-    // concatenate wav files and put a cue point at each boundary
-
-    let numSamples = 0
-    let offset = 0
-
-    const maxChannels = files.reduce((max, file) => {
-      const wav = new wavefile.WaveFile(fs.readFileSync(file))
-      return Math.max(max, wav.fmt.numChannels)
-    }, 1)
-
-    const chunks = files.map(file => {
+const getInfoAndChunks = (files, bitDepth, sampleRate) => {
+    return files.reduce((acc, file) => {
         const wav = new wavefile.WaveFile(fs.readFileSync(file))
         wav.toBitDepth(bitDepth)
         wav.toSampleRate(sampleRate)
         const samples = wav.getSamples(false)
         const chunk = wav.fmt.numChannels == 2 ? [samples[0], samples[1]] : [samples, samples]
-        numSamples += chunk[0].length
-        return chunk
-    })
+        return {
+            maxChannels: Math.max(acc.maxChannels, wav.fmt.numChannels),
+            maxSamples: Math.max(acc.maxSamples, chunk[0].length),
+            totalSamples: acc.totalSamples += chunk[0].length,
+            chunks: [...acc.chunks, chunk],
+        }
+    }, {maxChannels: 0, maxSamples: 0, totalSamples: 0, chunks:[]})
+}
+
+const cuecat = (files, bitDepth='16', sampleRate=44100.0) => {
+    // concatenate wav files and put a cue point at each boundary
+    let offset = 0
+    const {
+        maxChannels,
+        maxSamples,
+        totalSamples,
+        chunks,
+    } = getInfoAndChunks(files, bitDepth, sampleRate)
 
     const samples = chunks.reduce((samples, chunk) => {
         samples[0].set(chunk[0], offset)
@@ -31,18 +36,17 @@ const cuecat = (files, bitDepth='16', sampleRate=44100.0) => {
         }
         offset += chunk[0].length
         return samples
-    }, [new Float64Array(numSamples), new Float64Array(numSamples)])
-
+    }, [new Float64Array(totalSamples), new Float64Array(totalSamples)])
+    const wav_samples = maxChannels == 1 ? [samples[0]] : [samples[0], samples[1]]
     const wav = new wavefile.WaveFile()
-    const wav_samples = maxChannels == 1 ? [samples[0]] : [samples[0],samples[1]]
     wav.fromScratch(maxChannels, sampleRate, bitDepth, wav_samples)
 
     // set cue points (not regions)
-    offset = 0
-    chunks.forEach(chunk => {
+    chunks.reduce((offset, chunk) => {
         wav.setCuePoint({position: offset / sampleRate * 1000}) // milliseconds
-        offset += chunk[0].length
-    })
+        return offset + chunk[0].length
+    }, 0)
+
     return wav
 }
 
@@ -73,3 +77,8 @@ if (require.main === module) {
 }
 
 module.exports = cuecat
+
+module.exports = {
+    cuecat,
+    getInfoAndChunks
+};
--- test/tests.js
+++ test/tests.js
@@ -3,18 +3,33 @@ const fs = require("fs");
 const path = require("path");
 const {expect} = require("chai");
 
-const cuecat = require("../cuecat");
+const {cuecat, getInfoAndChunks} = require("../cuecat");
+
+const testDir = "./test/CR78"
+const files = fs.readdirSync(testDir)
+    .filter(f => path.extname(f).toLowerCase() === ".wav")
+    .map(f => `${testDir}/${f}`)
 
 describe("cuecat", () => {
     it("should not change output unexpectedly", () => {
-        const testDir = "./test/CR78"
-
-        const files = fs.readdirSync(testDir)
-            .filter(f => path.extname(f).toLowerCase() === ".wav")
-            .map(f => `${testDir}/${f}`)
         const wav = cuecat(files)
         const hash = crypto.createHash('md5')
             .update(wav.toBuffer()).digest("hex")
         expect(hash).to.equal("9228fc19a3cec3518a177d31bc0d8c74")
     })
 });
+
+describe("getInfoAndChunks", () => {
+    it("should calculate max and get chunks", () => {
+        const {
+            maxChannels,
+            maxSamples,
+            totalSamples,
+            chunks,
+        } = getInfoAndChunks(files, '16', 44100.0)
+        expect(maxChannels).to.equal(1)
+        expect(maxSamples).to.equal(27082)
+        expect(totalSamples).to.equal(219311)
+        expect(chunks).to.have.length(16)
+    })
+})
